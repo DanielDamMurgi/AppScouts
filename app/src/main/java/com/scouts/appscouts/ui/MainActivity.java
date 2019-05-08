0515915a896d1d3ee56019b09e07a8dc50f7567b
dani <daniel.ruedalao@gmail.com>
2019-05-08 17:26:30 +0200

    Login 1

--- app/src/main/java/com/scouts/appscouts/ui/MainActivity.java
+++ app/src/main/java/com/scouts/appscouts/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.scouts.appscouts;
+package com.scouts.appscouts.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.scouts.appscouts.R;
 
 public class MainActivity extends AppCompatActivity {
 
