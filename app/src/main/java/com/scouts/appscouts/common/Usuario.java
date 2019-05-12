6700ef30643a5ccc2496d7b37200942bec320033
dani <daniel.ruedalao@gmail.com>
2019-05-12 20:19:49 +0200

    nav_header

--- app/src/main/java/com/scouts/appscouts/common/Usuario.java
+++ app/src/main/java/com/scouts/appscouts/common/Usuario.java
@@ -4,6 +4,7 @@ public class Usuario {
     private int id;
     private String email;
     private String nombre;
+    private String apellidos;
     private String password;
     private boolean validado;
 }
