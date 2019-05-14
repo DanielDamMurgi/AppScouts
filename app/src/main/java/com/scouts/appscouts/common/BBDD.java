1290187083c4d04babba3a202ec5075222968c72
Equipo <daniel.ruedalao@gmail.com>
2019-05-14 14:16:22 +0200

    crashlytics1

--- app/src/main/java/com/scouts/appscouts/common/BBDD.java
+++ app/src/main/java/com/scouts/appscouts/common/BBDD.java
@@ -4,4 +4,5 @@ public class BBDD {
     private final String DIRECCION= "jdbc:mysql://sql7.freesqldatabase.com:3306/sql7291737";
     private final String USUARIO= "sql7291737";
     private final String CONTRASEÑA= "ZynRmi9UFj";
+
 }
